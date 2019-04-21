e524c66b8a4b7eb111c4d5f1b2aa10d856889858
Amitoj Kaur Chawla <amitoj1606@gmail.com>
2019-04-21 17:03:34 -0400

    Apr20: Todo

---
+++ apr20.cpp
@@ -0,0 +1,21 @@
+/*
+Max Rectangle in a Histogram
+We're given a histogram like the following, where contiguous (sharing a common border or touching) bars are made up of different heights. Let's assume all bars have the same width.
+
+The histogram is converted to an array of the heights of the bars:
+
+const histArr = [3, 1, 4, 2, 2, 1]
+
+With this knowledge, can we find the largest rectangular area within the histogram? 
+*/
+
+//Hint: Use a stack
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+}
